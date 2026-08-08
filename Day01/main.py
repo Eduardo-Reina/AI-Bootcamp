a6686b5ad8e482049e490b0dc924b24a7c61faa2
Eduardo Reina <diabloking@yahoo.com>
2026-08-08 19:47:59 -0400

    Complete Day 1 Python basics

---
+++ Day01/main.py
@@ -0,0 +1,87 @@
+# Ask For Name and Say hello and return name
+"""print("Hello, AI Engineers!")
+NewName = input("What's your name?: ")
+print ("Hello: " + NewName)"""
+
+# This is my first program using f(string)
+# Using user input
+# learning how how strip().title()work
+"""new_name = input("What's your name? ")
+new_age = input("How old are you? ")
+new_game = input("What is your favorite game? ",)
+
+print(f"Hello, {new_name}!")
+print("You are", new_age, "years old")
+print("Your favorite game is", new_game)
+print("Have a great day !")"""
+
+"""print("====================================")
+print("Welcome to the AI Bootcamp!")
+print("====================================")
+
+new_name = input("What is your full name? ")
+new_age = input("How old are you? ")
+new_place = input("Where are you from? ")
+new_major = input("What is your major? ")
+new_job = input("What is your dream job? ")
+new_language = input("What is your favorite programming language? ")
+
+
+new_name = new_name.strip().title()
+new_age = new_age.strip()
+new_place = new_place.strip().title()
+new_major = new_major.strip().title()
+new_job = new_job.strip().title()
+new_language = new_language.strip().title()
+
+first_name, last_name = new_name.split(" ")
+
+print("====================================")
+print("New Bootcamp Profile")
+print("====================================")
+
+print(f"Hello: {last_name}")
+print(f"Age: {new_age}")
+print(f"Place of birth: {new_place}")
+print(f"Major: {new_major}")
+print(f"Job: {new_job}")
+print(f"Favorite Language: {new_language}")"""
+
+# This is my basic math small program
+"""x = float(input("x = "))
+
+y = float(input("y = "))
+
+# round if you have 5.3 it goes to the closest int
+# if 8.5 instead of rounding up it will round to 8 because 
+# python rounds to the nerest even number
+print(round(x + y,2))
+print(round(x / y,2))
+print(round(x - y,2))
+print(round(x * y,2))
+print(round(x % y,2))"""
+
+# This is how to invent your own functions with def:define
+
+
+"""def hello(person):
+    print("Hello,", person)
+
+
+new_name = input("What's your name? ")
+
+hello(new_name)"""
+
+# how to make a small calculation that sguares numbers
+
+
+"""def main():
+    x = int(input("What's x? "))
+    print("x sguared is ", sguared(x))
+
+
+def sguared(n):
+    return (n * n,)
+
+
+main()"""
---
+++ Day01/notes.md
@@ -0,0 +1,24 @@
+# Day 1 Reflection
+
+Today I learned what f-strings are and how to use them.
+
+I learned how to ask the user for input and print the information back to them.
+
+I had done a little Python before, but I had forgotten most of the basics.
+
+One thing that made me really happy was writing:
+
+new_name = input(...)
+
+by myself before the instructor showed how to do it.
+
+I got stuck, made a few mistakes, and eventually figured it out on my own.
+
+That made me feel proud because it reminded me that I can solve problems if I keep trying.
+
+I also had an error pop up because I wrote the corect code in the wrong seguence.
+
+I did not notice it doring the frustration of not finding the error. need to read and look,
+more closely to what i write.
+
+
